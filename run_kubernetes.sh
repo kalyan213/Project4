6ab6dcecef63ee4506baaa53f5b4cdd385a5e0b0
kalyan213 <kalyan.fine@gmail.com>
2022-11-20 15:42:48 +0000

    commited run_docker and commit files...

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,6 +5,8 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=kalyan212/udaproject4
+kubectl run latest --image=$dockerpath --port=80
 
 # Step 2
 # Run the Docker Hub container with kubernetes
@@ -12,7 +14,8 @@
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward latest 8000:80
